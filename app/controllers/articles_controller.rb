848bfb73ad54f3294ffea990ca33034a471f6b0b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-01-23 22:54:35 -0300

    feat: Show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article  = Article.find(params[:id])
   end
 
   # GET /articles/new
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [ :show ]
 end
