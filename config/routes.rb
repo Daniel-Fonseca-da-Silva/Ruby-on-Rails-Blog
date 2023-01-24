b2bd637fe949f829699031a7c54a1221201c0a61
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-01-24 20:22:55 -0300

    feat: Article index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [ :show ]
+  resources :articles, only: [ :show, :index ]
 end
