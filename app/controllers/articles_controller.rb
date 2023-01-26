de589aa543b0822edeeeedae5d07807fb2205039
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-01-26 19:56:39 -0300

    feat: Validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,23 +23,13 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created with sucess!"
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-
-  # def create
-  #   @article = Article.new(article_params)
-
-  #   respond_to do |format|
-  #     if @article.save
-  #       format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-  #       format.json { render :show, status: :created, location: @article }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @article.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
