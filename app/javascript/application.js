c66330b3c3b366496bd3dee4e4b65c1340c3cc4c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-01-29 17:25:26 -0300

    style: Added icons to navigation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
